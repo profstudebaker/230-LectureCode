72c2387bf745be368af1e2b5fbac2e0a8eda0f48
Murphy <studebaker@chapman.edu>
2023-11-02 11:20:05 -0700

    added starter code for MP5

---
+++ 07_dictionaries_sets/passwords.py
@@ -0,0 +1,29 @@
+# Starter code for assignment MP5: Password Manager
+def swap_dict(encrypt_dict):
+    # TODO: Finish this function
+    pass # delete this after you have completed the function
+
+def encrypt(input_string, mapping_dict):
+    # TODO: Finish this function
+    pass # delete this after you have completed the function
+  
+def add_record(service, username, password, password_dict, encrypt_dict):
+    # TODO: Finish this function
+    pass # delete this after you have completed the function
+  
+def view_password(service, username, password_dict, decrypt_dict):
+    # TODO: Finish this function
+    pass # delete this after you have completed the function
+  
+
+def update_password(service, username, new_password, password_dict, encrypt_dict):
+    # TODO: Finish this function
+    pass # delete this after you have completed the function
+  
+
+if __name__ == "__main__":
+    password_dict = {}
+    # TODO: Create your initial mappings 
+    # of each letter in the alphabet to another letter
+    encrypt_dict = {}
+    # TODO: Create your main program with user input here
